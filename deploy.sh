8a2eb463181d6bd84ea8c582209453a18a3a38c9
Rudolf M. Schreier <rudolfschreier@gmx.de>
2019-03-05 19:15:15 +0100

    Take revision as param instead of env variable (#3)

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,9 @@ source beevenueenv/bin/activate
 pip install -r requirements.txt
 pip install -r requirements.linuxonly.txt
 
-sed -i 's/\(COMMIT_ID = \)\(.*\)/\1\"'${REVISION:0:8}'\"/g' beevenue_config.prod.py
+# First parameter to this shell script must be the commit_id
+# which we shorten to 8 characters.
+sed -i 's/\(COMMIT_ID = \)\(.*\)/\1\"'${1:0:8}'\"/g' beevenue_config.prod.py
 
 echo "Running migrations"
 /bin/bash ./flask.prod.sh db upgrade
