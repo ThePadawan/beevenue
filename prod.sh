432d61368306786a1974c2df6cc8a42ab98949e1
Rudolf M. Schreier <rudolfschreier@gmx.de>
2020-02-15 15:06:49 +0100

    Longer gunicorn timeout in prod.sh

--- prod.sh
+++ prod.sh
@@ -2,4 +2,5 @@
 set -ev
 source beevenueenv/bin/activate
 p=`realpath beevenue_config.prod.py`
-env BEEVENUE_CONFIG_FILE=$p gunicorn -b 0.0.0.0:7000 main:app
+# Some requests may run longer than the default timout of 30s
+env BEEVENUE_CONFIG_FILE=$p gunicorn --timeout 90 -b 0.0.0.0:7000 main:app
