de30fd860250d1368ea8585a1d4517649e8396d4
Rudolf M. Schreier <thepadawan3@gmail.com>
2020-10-16 15:20:25 +0200

    Update to Python 3.9 (#130)
    * Update Pillow to 8.0.0
    
    * Change to gunicorn3
    
    * Use local gunicorn instead of global

--- prod.sh
+++ prod.sh
@@ -3,4 +3,4 @@ set -ev
 source beevenueenv/bin/activate
 p=$(realpath beevenue_config.prod.py)
 # Some requests may run longer than the default timout of 30s
-env BEEVENUE_CONFIG_FILE="$p" gunicorn --workers 4 --timeout 90 -b 0.0.0.0:7000 main:app
+env BEEVENUE_CONFIG_FILE="$p" ./beevenueenv/bin/gunicorn --workers 4 --timeout 90 -b 0.0.0.0:7000 main:app
